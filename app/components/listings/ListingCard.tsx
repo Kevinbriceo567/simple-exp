66e7ac56e6bf13a25e3f1f8011bf3fee68ffb21b
kevinbriceo567 <kevinbriceo567@gmail.com>
2023-04-20 21:21:22 -0400

    Experience card

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -112,7 +112,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
           {location?.region}, {location?.label}
         </div>
         <div className="font-light text-neutral-500">
-          {reservationDate || data.categoty}
+          {reservationDate || data.category}
         </div>
         <div className="flex flex-row items-center gap-1">
           <div className="font-semibold">
